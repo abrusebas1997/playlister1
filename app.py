0a8fd255e4e1d9bfcebe0da1c077e331388cfb3e
Sebastian Abarca <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-10-02 18:30:36 -0700

    finished

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ comments = db.comments
 
 app = Flask(__name__)
 
-# OUR MOCK ARRAY OF PROJECT
+# OUR MOCK ARRAY OF PROJECTs
 # playlists = [
 #     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
 #     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
